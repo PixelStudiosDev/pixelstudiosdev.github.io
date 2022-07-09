a8152222246f62506530ea5d2d035f2831e929b5
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2022-07-09 16:32:05 +0200

    change light mode color

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,17 +13,10 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CubeCrafter', // Usually your GitHub org/user name.
-  projectName: 'cubecrafter.github.io', // Usually your repo name.
+  organizationName: 'CubeCrafter', 
+  projectName: 'cubecrafter.github.io', 
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -36,11 +29,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           routeBasePath: '/',
-          editUrl:
-            'https://github.com/CubeCrafter/cubecrafter.github.io/tree/master/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
