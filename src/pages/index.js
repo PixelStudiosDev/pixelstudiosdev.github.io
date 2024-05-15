a54bcdc10151d4a2a8845596faf638a64b72d5e2
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2024-05-15 18:27:42 +0200

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,37 +7,42 @@ import Layout from '@theme/Layout';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+    const {siteConfig} = useDocusaurusContext();
 
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <img src="img/logo.png" width="200px" />
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/wool-wars">
-            Wool Wars
-          </Link> 
-          <Link
-            className="button button--secondary button--lg"
-            to="/guilds">
-            Guilds
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <img src="img/logo.png" width="200px"/>
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/achievements">
+                        Achievements
+                    </Link>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/wool-wars">
+                        Wool Wars
+                    </Link>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/guilds">
+                        Guilds
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  return (
-    <Layout
-      title={`Pixel Studios Wiki`}
-      description="Documentation for our plugins.">
-      <HomepageHeader />
-    </Layout>
-  );
+    return (
+        <Layout
+            title={`Pixel Studios Wiki`}
+            description="Documentation for our plugins.">
+            <HomepageHeader/>
+        </Layout>
+    );
 }
