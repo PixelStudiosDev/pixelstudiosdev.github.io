c46f475e5b2db1fe44c6ac6f7fef3739afb4906d
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2024-06-23 11:13:06 +0200

    update developer api

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['java'],
+        additionalLanguages: ['java', 'groovy'],
       },
       colorMode: {
         defaultMode: 'dark',
